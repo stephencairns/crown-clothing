733600f4a75d9a2f22c9e058900566ee7ce8719b
Stephen Cairns <stephen.cairns@olmgroup.com>
2022-02-18 16:59:54 +0000

    Update menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Routes, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1> Hats Page here</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="shop/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
